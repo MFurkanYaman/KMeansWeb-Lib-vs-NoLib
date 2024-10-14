bac8abc7e630be0422c846b4ac72d82009410790
Metin Furkan Yaman <96432415+MFurkanYaman@users.noreply.github.com>
2024-10-14 12:04:08 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Docker içinde dışarıya açılmasını sağlar
+    port: 5173, // Port ayarını doğru tanımla
+    strictPort: true, // 5173 kullanılamazsa hata versin
+  },
+});
